def5bfbbc513fe9fd7f7dd5c2e056b5ab6124d7a
Jeetendra chauhan <jeet4code@gmail.com>
2022-12-04 12:06:36 +0530

    change customer constructor to take id field

--- src/main/java/customerManagement/Domain/Customer.java
+++ src/main/java/customerManagement/Domain/Customer.java
@@ -9,13 +9,15 @@ public class Customer {
     public Address address;
     public List<ProductId> cart;
     public List<ProductId> wishList;
+    public int id;
 
-    private Customer(String name,
-    Email email,
-    Phone phone,
-    Address address,
-    List<ProductId> cart,
-    List<ProductId> wishList) {
+    private Customer(int id, String name,
+            Email email,
+            Phone phone,
+            Address address,
+            List<ProductId> cart,
+            List<ProductId> wishList) {
+                this.id = id
         this.name = name;
         this.email = email;
         this.phone = phone;
@@ -25,11 +27,20 @@ public class Customer {
     }
 
     public static Customer create(String name,
-    Email email,
-    Phone phone,
-    Address address,
-    List<ProductId> cart,
-    List<ProductId> wishList) {
-        return new Customer(name, email, phone, address, cart, wishList);
+            Email email,
+            Phone phone,
+            Address address,
+            List<ProductId> cart,
+            List<ProductId> wishList) {
+        return new Customer(-1, name, email, phone, address, cart, wishList);
+    }
+
+    public static Customer create(int id, String name,
+            Email email,
+            Phone phone,
+            Address address,
+            List<ProductId> cart,
+            List<ProductId> wishList) {
+        return new Customer(id, name, email, phone, address, cart, wishList);
     }
 }
